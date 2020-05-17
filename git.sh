0aa8b97a671a7e3003b91c3f7932583506820e2a
benjamin <bappiahpoku@gmail.com>
2020-05-17 12:10:41 +0000

    update commit 5

--- git.sh
+++ git.sh
@@ -5,13 +5,15 @@ git add .;
 git commit -m "update commit $1";
 echo "Files uploaded to local repository"
 
+sleep 1
 
 # Git push to the remote repository 
 if git push origin master
 then
-    git pull orgin master
     echo "Files uploaded to Github"
+    sleep 1 
+    git pull origin master
+    echo "Files synced with Github"
 else
     echo "Unable to upload files to Github"
 fi
-
