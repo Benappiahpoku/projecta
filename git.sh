e3ca9495b62250215dc7f334e947d1f151092a2a
benjamin <bappiahpoku@gmail.com>
2020-05-17 12:04:28 +0000

    update commit 4

--- git.sh
+++ git.sh
@@ -6,9 +6,10 @@ git commit -m "update commit $1";
 echo "Files uploaded to local repository"
 
 
-# Git pull to sync loacal and remote repository 
-if git pull origin master
+# Git push to the remote repository 
+if git push origin master
 then
+    git pull orgin master
     echo "Files uploaded to Github"
 else
     echo "Unable to upload files to Github"
--- github.sh
+++ github.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if git pull origin master
+if git push origin master
 then
     echo "Files uploaded to Github"
+    sleep 1 
+    git pull origin master
+    echo "Files synced with Github"
 else
     echo "Unable to upload files to Github"
 fi
